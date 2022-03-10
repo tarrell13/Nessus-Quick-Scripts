b9b9f5fead6b60dd59c56560e6a6f90db18fc5d6
Tarrell Fletcher <tarrell13@verizon.net>
2022-03-10 18:02:51 -0500

    Fixed Merge Conflicts

--- NessExporter/ness-exporter.py
+++ NessExporter/ness-exporter.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
-=======
 #!/usr/bin/python3
->>>>>>> dev
-
 
 import argparse
 import time
@@ -50,35 +45,20 @@ else:
 
 # Function is used to login to the Nessus Instance
 def nessus_login():
-<<<<<<< HEAD
 
-    print(("[*] Log into Nessus Instance => %s" %NESSUS_INSTANCE))
-    print(("="*len("[*] Log into Nessus Instance => %s" %NESSUS_INSTANCE))) 
-
-=======
     print("[*] Log into Nessus Instance => %s" % NESSUS_INSTANCE)
     print("=" * len("[*] Log into Nessus Instance => %s" % NESSUS_INSTANCE))
 
->>>>>>> dev
     while True:
         username = input("[*] Username: ")
         password = getpass.getpass("[*] Password: ")
 
-<<<<<<< HEAD
-        login_request = requests.post(NESSUS_INSTANCE+"/session", data={"username": username, "password": password}, verify=False) 
-
-        if login_request.status_code == 200:
-            print("[+] Authentication Successful")
-            time.sleep(0.5)        
-=======
         login_request = requests.post(NESSUS_INSTANCE + "/session", data={"username": username, "password": password},
                                       verify=False)
 
         if login_request.status_code == 200:
             print("[+] Authentication Successful")
             time.sleep(0.5)
->>>>>>> dev
-
             return json.loads(login_request.content)["token"]
         else:
             print("[!] Check Authentication")
@@ -89,18 +69,6 @@ def nessus_login():
 def retrieve_scans(folderid=None):
     scan_list = {}
 
-<<<<<<< HEAD
-    scan_list = {}
-
-    if FOLDER:
-        scans = json.loads(requests.get(NESSUS_INSTANCE + "/scans", verify=False, data={"folder_id":folderid},headers=HEADERS).content)["scans"]
-    else:
-        scans = json.loads(requests.get(NESSUS_INSTANCE+"/scans", verify=False, headers=HEADERS).content)["scans"]
-
-    for scan in scans:
-        if scan["status"] != "empty":
-            scan_list[scan["name"].encode()] = scan["id"]
-=======
     if FOLDER:
         scans = json.loads(requests.get(NESSUS_INSTANCE + "/scans", verify=False, data={"folder_id": folderid},
                                         headers=HEADERS).content)["scans"]
@@ -110,7 +78,6 @@ def retrieve_scans(folderid=None):
     for scan in scans:
         if scan["status"] != "empty":
             scan_list[scan["name"]] = scan["id"]
->>>>>>> dev
 
     return scan_list
 
@@ -118,24 +85,6 @@ def retrieve_scans(folderid=None):
 def retrieve_export_request(scan_list):
 
     increment = 0
-<<<<<<< HEAD
-
-    print(("[*] Save Directory: %s" %PATH))
-    print(("[*] Formats => " + str(FORMATS)))
-    print("[*] Downloading Files....")
-
-    for name,id in scan_list.items():
-        for format in FORMATS:  
-            if format == "nessus" or format == "csv":
-                export_request = json.loads(requests.post(NESSUS_INSTANCE+"/scans/%s/export" %str(id), headers=HEADERS, verify=False, data=json.dumps({"format": format})).content)["token"]
-            elif format == "pdf" or format == "html":
-                export_request = json.loads(requests.post(NESSUS_INSTANCE+"/scans/%s/export" %str(id), headers=HEADERS, verify=False, data=json.dumps({"format" : format, "chapters" : "vuln_hosts_summary"})).content)["token"]
-
-            export_request_check(name,id,export_request, format)
-
-            increment += 1
-            progress.progress(increment, len(scan_list))
-=======
 
     print("[*] Save Directory: %s" % PATH)
     print("[*] Formats => " + str(FORMATS))
@@ -148,7 +97,6 @@ def retrieve_export_request(scan_list):
                 export_request = json.loads(requests.post(NESSUS_INSTANCE + "/scans/%s/export" % str(id), headers=HEADERS, verify=False, data=json.dumps({"format": format})).content.decode())
             elif format == "pdf" or format == "html":
                 export_request = json.loads(requests.post(NESSUS_INSTANCE + "/scans/%s/export" % str(id), headers=HEADERS, verify=False,data=json.dumps({"format": format, "chapters": "vuln_hosts_summary"})).content.decode())
->>>>>>> dev
 
             export_request_check(name, id, export_request, format)
 
@@ -156,15 +104,6 @@ def retrieve_export_request(scan_list):
             increment += 1
             progress.progress(increment, len(FORMATS) * len(scan_list))
 
-<<<<<<< HEAD
-    while True:
-        if json.loads(requests.get(NESSUS_INSTANCE+"/tokens/%s/status" %(str(file_request)), verify=False, headers=HEADERS).content)["status"] == "ready":
-            download_file_request(name,id,file_request,format)
-            break
-        else:
-            time.sleep(1)
-=======
->>>>>>> dev
 
 def export_request_check(name, id, file_request, format):
     while True:
@@ -174,29 +113,7 @@ def export_request_check(name, id, file_request, format):
         else:
             time.sleep(1)
 
-<<<<<<< HEAD
-    download = requests.get(NESSUS_INSTANCE+"/tokens/%s/download" %(str(file_request)), verify=False, headers=HEADERS)
-
-    if format == "nessus":
-        with open(PATH+"/%s.nessus" %name.decode(), "wb") as handle:
-            for chunk in download.iter_content(chunk_size=128):
-                handle.write(chunk)
-    elif format == "html":
-        with open(PATH+"/%s.html" %name.decode(), "wb") as handle:
-            for chunk in download.iter_content(chunk_size=128):
-                handle.write(chunk)
-    elif format == "csv":
-        with open(PATH+"/%s.csv" %name.decode(), "wb") as handle:
-            for chunk in download.iter_content(chunk_size=128):
-                handle.write(chunk)
-    elif format == "pdf":
-        with open(PATH+"/%s.pdf" %name.decode(), "wb") as handle:
-            for chunk in download.iter_content(chunk_size=128):
-                handle.write(chunk)
-=======
-
 def download_file_request(name, id, file_request, format):
->>>>>>> dev
 
     download = requests.get(NESSUS_INSTANCE + "/scans/%s/export/%s/download" % (str(id), str(file_request["file"])), verify=False, headers=HEADERS)
 
@@ -216,38 +133,6 @@ def download_file_request(name, id, file_request, format):
         with open(PATH + "/%s.pdf" % name, "wb") as handle:
             for chunk in download.iter_content(chunk_size=128):
                 handle.write(chunk)
-
-<<<<<<< HEAD
-    folder_list = []
-    dump = json.loads(requests.get(NESSUS_INSTANCE+"/folders",verify=False,headers=HEADERS).content)["folders"]
-
-    for folder in dump:
-        if folder["type"] == "trash":
-            continue
-        else:
-            folder_list.append(folder)
-
-    print(("="*len("NESSUS FOLDERs")))
-    print("NESSUS FOLDERS ")
-    print(("="*len("NESSUS FOLDERS")))
-
-    for folder in folder_list:
-        print(("ID [ %s  ] :: %s" %(folder["id"], folder["name"])))
-
-    found = False
-    while found is False:
-
-        choice = input("Choose Folder ID: ")
-        for folder in folder_list:
-            if str(folder["id"]) == choice:
-                found = True
-
-        if found is False:
-            print("(!) Invalid Selection")
-
-    print("\n")
-    return choice
-=======
 
 def RetrieveFolderChoice():
     folder_list = []
@@ -276,25 +161,10 @@ def RetrieveFolderChoice():
 
         if found is False:
             print("(!) Invalid Selection")
->>>>>>> dev
 
     print("\n")
     return choice
 
-<<<<<<< HEAD
-    global HEADERS
-
-    HEADERS = {"X-Cookie" : "token=%s" %nessus_login(),
-            "Content-Type" : "application/json"}
-
-    if FOLDER:
-        scans = retrieve_scans(RetrieveFolderChoice())
-    else:
-        scans = retrieve_scans()
-
-    retrieve_export_request(scans)
-    print("\n[+] Downloads Complete")
-=======
 
 def main():
     global HEADERS
@@ -306,7 +176,6 @@ def main():
         scans = retrieve_scans(RetrieveFolderChoice())
     else:
         scans = retrieve_scans()
->>>>>>> dev
 
     retrieve_export_request(scans)
     print("\n[+] Downloads Complete")
